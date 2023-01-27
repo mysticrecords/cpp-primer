e1178f434618a43d5644aea1648ab5508df35d19
Matt Walsh <mysticrecords@hotmail.com>
2023-01-27 19:16:53 +0000

    completing exercise 1.5

---
+++ Exercise_Section_1.1.1/Exercise_1.5/exercise_1.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+     int num1 = 10;
+    int num2 = 20;
+
+    std::cout << num1;
+    std::cout << " * ";
+    std::cout << num2;
+    std::cout << " = ";
+    std::cout << num1 * num2;
+    std::cout << std::endl;
+
+    return 0;
+}
